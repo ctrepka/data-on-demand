c27eb4a1d8530fd49ecc83fc7744377a871bd9e3
ctrepka <chris.repka@twdb.texas.gov>
2024-08-19 14:49:32 -0500

    init

---
+++ data/get_started.sh
@@ -0,0 +1,6 @@
+## get dataset from data.geographic.gov for statewide address points and extract
+wget -O address_points_2024.zip https://data.tnris.org/6d9c4a2e-b5bb-49b3-9ceb-0727f4711c5b/resources/stratmap24-addresspoints_48_ap.zip
+
+unzip address_points_2024.zip
+
+
