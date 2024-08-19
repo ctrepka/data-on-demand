f8e23be0b88b6d4d58a25d6bc4b47ad6cf265643
ctrepka <chris.repka@twdb.texas.gov>
2024-08-19 18:41:31 -0500

    Dockerfile for retrieving and converting address pts 2024 dataset to parquet; TODO: sveltekit app serving over 5173

--- data/get_started.sh
+++ data/get_started.sh
@@ -3,4 +3,10 @@ wget -O address_points_2024.zip https://data.tnris.org/6d9c4a2e-b5bb-49b3-9ceb-0
 
 unzip address_points_2024.zip
 
+ls -la
 
+sleep 5
+
+ogr2ogr /data/stratmap24-addresspoints_48.parquet /data/stratmap24-addresspoints_48.gdb
+
+duckdb -c "INSTALL spatial; LOAD spatial; SELECT geom FROM '/data/stratmap24-addresspoints_48.parquet LIMIT 24;" >> result.txt
